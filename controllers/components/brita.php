6801b596d2287ec492f0448a32da92afd00dcf77
Ben Rasmusen <benrasmusen@gmail.com>
2011-10-17 11:07:00 -0700

    Adjusting paths to the internal vendors file.

--- controllers/components/brita.php
+++ controllers/components/brita.php
@@ -1,5 +1,5 @@
 <?php  
-App::import('Vendor', 'HTMLPurifier', array('file' => 'htmlpurifier' . DS . 'library' . DS . 'HTMLPurifier.auto.php'));
+App::import('Vendor', 'Brita.HTMLPurifier', array('file' => 'htmlpurifier' . DS . 'library' . DS . 'HTMLPurifier.auto.php'));
 
 class BritaComponent extends Object { 
 
@@ -11,22 +11,22 @@ class BritaComponent extends Object {
 	
 	public $tidy_level = 'heavy';
 	
-	public $doctype = 'XTML 1.0 Transitional';
+	public $doctype = 'XHTML 1.0 Transitional';
 	
 	public $encoding = 'ISO-8859-1';
 
 	function initialize(&$Controller) { 
-
+		
         //the next few lines allow the config settings to be cached 
         $config = HTMLPurifier_Config::createDefault(); 
-        $config->set('HTML', 'DefinitionID', $this->definition_id); 
-        $config->set('HTML', 'DefinitionRev', $this->definition_revision); 
+        $config->set('HTML.DefinitionID', $this->definition_id); 
+        $config->set('HTML.DefinitionRev', $this->definition_revision); 
         //levels describe how aggressive the Tidy module should be when cleaning up html 
         //four levels: none, light, medium, heavy 
-        $config->set('HTML', 'TidyLevel', $this->tidy_level); 
+        $config->set('HTML.TidyLevel', $this->tidy_level); 
         //check the top of your html file for the next two 
-        $config->set('HTML', 'Doctype', $this->doctype); 
-        $config->set('Core', 'Encoding', $this->encoding); 
+        $config->set('HTML.Doctype', $this->doctype); 
+        $config->set('Core.Encoding', $this->encoding); 
          
         //BritaComponent instance of controller is replaced by a htmlpurifier instance 
         $Controller->Brita =& new HTMLPurifier($config); 
